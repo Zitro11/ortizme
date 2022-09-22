75c2e0a47c36e436f1a5d55d8ff5278d0750da8e
Zitro11 <61438031+Zitro11@users.noreply.github.com>
2022-09-21 19:05:33 -0500

    commit

--- Parcial 1/Server/Cors/ServerCors.js
+++ Parcial 1/Server/Cors/ServerCors.js
@@ -6,7 +6,8 @@ const app = express()
 app.use(cors({origin:'http://127.0.0.1:5500'}))
 
 app.get('/',(req, res)=>{
-    res.send("Servidor express contestando a puerto 8081")
+   // res.send("Servidor express contestando a puerto 8081")
+   res.send('.static/index.html',{root:__dirname})
 })
 
 app.post('/',(req, res)=>{
