11e29ef69202944049f8b931dcef90ea4091b9ce
Zitro11 <61438031+Zitro11@users.noreply.github.com>
2022-09-22 20:04:28 -0500

    commit

---
+++ Parcial 1/Server/Thunder/ServerCors.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(express.text())
+app.use(express.json())
+
+app.use(cors({origin:'http://127.0.0.1:5500'}))
+
+
+
+app.post('/texto',(req,res)=>{
+   console.log(req.body)
+   let may = req.body.toUpperCase()
+   let sinesp =req.body.trim()
+   let longi = req.body.length
+   res.json({
+      mayusculas: may,
+      sinespacios: sinesp,
+      longitud: longi
+   })
+})
+
+app.post('/json', (req,res) => {
+   console.log(req.body.nombre)
+   let cadena = "Hola " +req.body.nombre+""+req.body.apellido+" como estas"
+   req.json({saludo:cadena})
+})
+
+app.get('/mayusculas/:cadena', (req,res) => {
+   console.log(req.params)
+   res.send(req.params)
+})
+
+app.get('suma', (req,res) => {
+   console.log(req.query)
+   let suma = parseInt(req.query.x)+parent(req.query.y)
+   res.send(
+   `La suma es ${suma}`)
+})
+
+
+
+
+app.get('/',(req, res)=>{
+    res.send("Servidor express contestando a puerto 8081")
+ })
+
+app.listen(8083,()=>{console.log('Server funcional')})
+
+app.get('/',(req, res)=>{
+   // res.send("Servidor express contestando a puerto 8081")
+   res.sendFile('/static/index.html',{root:__dirname})
+})
+
+app.use((req, res)=>{
+   res.sendFile('/static/NotFound.html',{root:__dirname})
+})
+
+
--- Parcial 1/Server/static/ServerCors.js
+++ Parcial 1/Server/static/ServerCors.js
@@ -4,19 +4,19 @@ const app = express()
 
 app.use(cors({origin:'http://127.0.0.1:5500'}))
 
-/*app.get('/',(req, res)=>{
+app.get('/',(req, res)=>{
     res.send("Servidor express contestando a puerto 8081")
- })*/
+ })
 
-app.listen(8081,()=>{console.log('Server funcional')})
+app.listen(8083,()=>{console.log('Server funcional')})
 
 app.get('/',(req, res)=>{
    // res.send("Servidor express contestando a puerto 8081")
-   res.sendFile('.server/.static/index.html',{root:__dirname})
+   res.sendFile('/index.html',{root:__dirname})
 })
 
 app.use((req, res)=>{
-   res.sendFile('.server/.static/NotFound.html',{root:__dirname})
+   res.sendFile('/NotFound.html',{root:__dirname})
 })
 
 
