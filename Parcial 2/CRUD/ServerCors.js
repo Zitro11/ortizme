e53dd74103a159f20bcb5e9ae44abef6f9ce0705
Zitro11 <61438031+Zitro11@users.noreply.github.com>
2022-10-19 22:32:28 -0500

    commit

---
+++ Parcial 2/CRUD/ServerCors.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express() 
+
+var j2x = require ('json2xls')
+var fs = require ('fs')
+var mysql = require('mysql')
+
+const { json, response } = require('express');
+
+var con = mysql.createConnection({
+    host : "localhost",
+    user: "root",
+    password : "",
+    database: "api"
+});
+
+app.use(cors({origin:'http://127.0.0.1:5500'}))
+
+app.use(express.json())
+app.use(express.text())
+
+//get
+app.get('/',(req, res)=>{
+    con.connect(function(err){
+        if(err) throw err;
+        console.log("conected!");
+    
+        miQuery = 'SELECT * FROM amigos WHERE numero ='+req.body
+    
+        con.query(miQuery, function (err, response){
+            console.log(response)
+            res.send(response)
+            con.end()
+        })
+    })
+})
+
+//post
+
+app.post('/',(req, res)=>{
+    con.connect(function(err){
+        if(err) throw err;
+        console.log("conected!");
+    
+        miQuery = (`INSERT INTO amigos (Numero, Nombre, Apodo) VALUES ('${Numero}'), ('${Nombre}'), ('${Apodo}')`)
+        con.query(miQuery, function (err, response){
+            console.log(response)
+            res.send(response)
+            con.end()
+        })
+    })
+})
+
+//Delete
+
+
+
+//Put patch
+
+
+
+app.listen(8081,()=>{console.log('Server funcional')})
